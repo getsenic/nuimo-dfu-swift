0f9c9d5ccdfd4c81e3a90b39eda025a5ca3e196e
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-02-21 17:49:30 +0100

    Clean code

--- Sources/NuimoDFUBluetoothController.swift
+++ Sources/NuimoDFUBluetoothController.swift
@@ -10,9 +10,6 @@ import CoreBluetooth
 import NuimoSwift
 
 public class NuimoDFUBluetoothController: NuimoBluetoothController {
-    override public var serviceUUIDs:                    [CBUUID]            { return [] }
-    override public var charactericUUIDsForServiceUUID:  [CBUUID : [CBUUID]] { return [:] }
-    override public var notificationCharacteristicUUIDs: [CBUUID]            { return [] }
     open override class var maxAdvertisingPackageInterval: TimeInterval? { return 1.0 }
 
     override public func connect(autoReconnect: Bool = false) {
